47985b5dff5950453465fccff9a8e2f5a9037b71
joris <joris9191@protonmail.com>
2025-05-12 19:35:40 +0200

    prob works?

--- drive_publisher/drive_publisher/drive_publisher.py
+++ drive_publisher/drive_publisher/drive_publisher.py
@@ -25,8 +25,7 @@ class DrivePublisher(Node):
         # Parameters
         self.declare_parameter('max_speed', 2.0)
         self.declare_parameter('max_steering_angle', 0.34)
-        self.declare_parameter('safety_distance', 0.5)  # Distance to start slowing down
-        self.declare_parameter('stop_distance', 0.01)    # Distance to completely stop
+        self.declare_parameter('safety_distance', 0.5)
         self.declare_parameter('angle_increment', 0.0058)  # LiDAR angle increment
         
         self.get_logger().info('Drive Publisher Node has been started')
@@ -42,11 +41,9 @@ class DrivePublisher(Node):
         max_speed = self.get_parameter('max_speed').value
         max_steering = self.get_parameter('max_steering_angle').value
         safety_distance = self.get_parameter('safety_distance').value
-        stop_distance = self.get_parameter('stop_distance').value
         
         # Calculate the best steering angle and speed
-        steering_angle, speed = self.calculate_drive_command(
-            ranges, max_speed, max_steering, safety_distance, stop_distance)
+        steering_angle, speed = self.calculate_drive_command(ranges, max_speed, max_steering, safety_distance)
         
         # Create and publish drive message
         drive_msg = AckermannDriveStamped()
@@ -57,50 +54,26 @@ class DrivePublisher(Node):
         
         self.drive_pub.publish(drive_msg)
 
-    def calculate_drive_command(self, ranges, max_speed, max_steering, safety_distance, stop_distance):
+    def calculate_drive_command(self, ranges, max_speed, max_steering, safety_distance):
         # Define the regions of interest in the LiDAR scan
-        # Using 360 degrees of the LiDAR, divided into 5 regions
-        num_points = len(ranges)
-        points_per_region = num_points // 5
+        # Front region (slightly wider than the car)
+        front_region = ranges[300:420]  # Approximately ±30 degrees in front
         
-        # Define regions (in degrees):
-        # Region 0: Far left (288-360 degrees)
-        # Region 1: Left (216-288 degrees)
-        # Region 2: Front (144-216 degrees)
-        # Region 3: Right (72-144 degrees)
-        # Region 4: Far right (0-72 degrees)
-        
-        regions = [
-            ranges[0:points_per_region],                    # Far right
-            ranges[points_per_region:2*points_per_region],  # Right
-            ranges[2*points_per_region:3*points_per_region], # Front
-            ranges[3*points_per_region:4*points_per_region], # Left
-            ranges[4*points_per_region:]                    # Far left
-        ]
-        
-        # Get minimum distances for each region
-        region_mins = [np.min(region) for region in regions]
-        
-        # Check if we need to stop completely
-        if any(d < stop_distance for d in region_mins):
-            return 0.0, 0.0  # Stop completely
-        
-        # Check if we need to slow down and turn
-        if any(d < safety_distance for d in region_mins):
-            # Find the region with the most space
-            best_region = np.argmax(region_mins)
+        # Check if there are obstacles in the front region
+        if np.min(front_region) < safety_distance:
+            # Obstacle detected, need to turn
+            # Find the direction with the most space
+            left_region = ranges[420:540]  # Left side
+            right_region = ranges[180:300]  # Right side
             
-            # Calculate steering angle based on the best region
-            if best_region == 0:  # Far right
-                steering_angle = -max_steering
-            elif best_region == 1:  # Right
-                steering_angle = -max_steering * 0.7
-            elif best_region == 2:  # Front
-                steering_angle = 0.0
-            elif best_region == 3:  # Left
-                steering_angle = max_steering * 0.7
-            else:  # Far left
-                steering_angle = max_steering
+            left_min = np.min(left_region)
+            right_min = np.min(right_region)
+            
+            # Turn towards the side with more space
+            if left_min > right_min:
+                steering_angle = max_steering  # Turn left
+            else:
+                steering_angle = -max_steering  # Turn right
             
             # Reduce speed when obstacle is close
             speed = max_speed * 0.5
